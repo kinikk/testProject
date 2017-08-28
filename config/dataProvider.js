44b9bbbca97022d8527836a60d031dfc49d7833e
Adam <adam@MacBook-Pro-Adam.local>
2017-08-28 21:03:26 +0200

    Web server api

--- config/dataProvider.js
+++ config/dataProvider.js
@@ -1,10 +1,17 @@
-var mysql = require('mysql'); 
+var sqlite3 = require('sqlite3').verbose();
 
-var connection = mysql.createConnection({ 
-      host     : 'localhost', 
-      user     : 'root', 
-      password : '',
-      database : 'customers'
-    } 
-);
-module.exports = connection;
+dataApi = ()=>{};
+
+
+dataApi.getData = (type, limit,offset)=>{
+  const db = new sqlite3.Database('./dataBase/chinook.db');
+  return new Promise((res,rej)=>{
+    let table = [];
+    db.all("SELECT * from "+type+" limit "+limit, function(err, data) {
+      res(data);
+    })
+  }).then(db.close());
+}
+
+
+module.exports = dataApi;
--- routes/api/provider.js
+++ routes/api/provider.js
@@ -1,27 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var http = require("http");
-var connection = require("../../config/dataProvider");
+var dataApi = require("../../config/dataProvider");
 /* GET users listing. */
 
 router.use('/provider/:method', function(req, res, next) {
-  var data= {
+  let data= {
   	"host" : req.query.host,
   	"gateway" : req.query.method,
   	"port" : req.query.port,
   	"data" : req.query.data.replace(/'/g,'"')
   }
-
+  let type = 'albums';
+  let limit = 10;
     data=JSON.parse(data.data);
-    var limit = 10;
-    if (data.limit)
-      limit=data.limit
-
-  connection.query('SELECT * FROM customers LIMIT '+limit,function(err,rows,fields){
-   
-    res.send(JSON.stringify(rows));
-
-  });
+    if (data.limit){
+      limit=data.limit;
+    } 
+    if (data.type){
+      type = data.type;
+    }
+    console.log(type)
+    dataApi.getData(type,limit,0).then(data=>{
+      res.send(JSON.stringify(data));
+    })
 });
 
 module.exports = router;
